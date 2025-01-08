74f45d37134948944c42655b09fbfba4ba26e9df
mandi7469 <mandi7469@aol.com>
2025-01-08 18:54:59 -0500

    inputted a navbar in the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // imports
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import "./main.css";
+import backgroundVideo from "../src/assets/backgroundVideo.mp4";
 
 function App() {
-  
-
   return (
     <>
-     <main>
-      <Outlet />
-     </main>
+      <video className="backgroundVideo" loop autoPlay muted>
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <main>
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
 import "./Home.css";
 
 const Home = () => {
-  
-
   return (
-   <div></div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar 
+      position="fixed"
+      sx={{
+        boxShadow: 0,
+        bgcolor: 'transparent',
+        backgroundImage: 'none',
+        mt: 'calc(var(--template-frame-height, 0px) + 28px)',
+      }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit" variant="text" size="small">Sign in</Button>
+          <Button color="primary" variant="contained" size="small">
+              Sign up
+            </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-}
+};
 
 export default Home;
